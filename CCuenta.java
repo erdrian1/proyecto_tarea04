94713eed42234b33b140615e7bc79ac76581dbe9
erdrian1 <159165449+erdrian1@users.noreply.github.com>
2024-02-07 17:29:13 +0100

    Update CCuenta.java

--- CCuenta.java
+++ CCuenta.java
@@ -1,40 +1,43 @@
+package cuentas;
+
+
+/**
+ * @author FRANCISCO ADRIÁN ZÁJARA PONCE
+ * La clase CCuenta representa una cuenta bancaria con funciones básicas como ingreso y retiro.
+ * Esta clase proporciona métodos para acceder y manipular la información de la cuenta.
+ */
 public class CCuenta {
 
-
+    /** El nombre del titular de la cuenta. */
     private String nombre;
+
+    /** El número de cuenta. */
     private String cuenta;
+
+    /** El saldo actual de la cuenta. */
     private double saldo;
-    private double tipoInterés;
 
-    public CCuenta()
-    {
+    /** El tipo de interés de la cuenta. */
+    private double tipoInteres;
+
+    /**
+     * Constructor predeterminado de la clase CCuenta.
+     * Crea una cuenta con valores iniciales predeterminados.
+     */
+    public CCuenta() {
     }
 
-    public CCuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-    }
-
-    public double estado()
-    {
-        return saldo;
-    }
-
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
-    }
-
-    public void retirar(double cantidad) throws Exception
-    {
-        if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+    /**
+     * Constructor de la clase CCuenta que permite establecer valores iniciales.
+     * @param nom     El nombre del titular de la cuenta.
+     * @param cue     El número de cuenta.
+     * @param sal     El saldo inicial de la cuenta.
+     * @param tipo    El tipo de interés de la cuenta.
+     */
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+        tipoInteres = tipo;
     }
 }
